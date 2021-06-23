5619fd0d9eff3ea7eb9ce3c4b8c3ae7598b0faf6
heyj <1961604257@qq.com>
2021-06-23 15:15:52 +0800

    test commit mywar

---
+++ src/test/java/com/example/mywar/MywarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mywar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MywarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
